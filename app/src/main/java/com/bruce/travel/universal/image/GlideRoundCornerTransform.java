20b81c3163ede8efab17365380e9cdfb6325fb72
qizhenghao <qizhenghao258@163.com>
2016-09-07 20:47:39 +0800

    add rounded corner transform

--- app/src/main/java/com/bruce/travel/universal/image/GlideRoundCornerTransform.java
+++ app/src/main/java/com/bruce/travel/universal/image/GlideRoundCornerTransform.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.graphics.RectF;
+import android.util.Log;
 
 import com.bruce.travel.universal.utils.Methods;
 import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
@@ -18,7 +21,7 @@ import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
  */
 public class GlideRoundCornerTransform extends BitmapTransformation {
 
-    private static float mRadius = Methods.computePixelsWithDensity(14);
+    private static float mRadius = Methods.computePixelsWithDensity(16);
 
     public GlideRoundCornerTransform(Context context) {
         super(context);
@@ -26,40 +29,40 @@ public class GlideRoundCornerTransform extends BitmapTransformation {
 
     @Override
     protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
-        return roundedCorner(pool, toTransform);
+        return roundedCorner(pool, toTransform, outWidth, outHeight);
     }
 
-    private static Bitmap roundedCorner(BitmapPool pool, Bitmap source) {
+    private static Bitmap roundedCorner(BitmapPool pool, Bitmap source, int outWidth, int outHeight) {
         if (source == null) return null;
-        int size = Math.min(source.getWidth(), source.getHeight());
-        Bitmap result = pool.get(size, size, Bitmap.Config.ARGB_8888);
-        if (result == null) {
-            result = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+
+        int sourceWidth = source.getWidth(), sourceHeight = source.getHeight();
+        float scale = Math.max(outWidth * 1.0f / sourceWidth, outHeight * 1.0f / sourceHeight);
+        Log.d("Bruce", "scale: " + scale + " outWidth : " + outWidth + " outHeight: " + outHeight + " source.getWidth(): " +  source.getWidth() + "source.getHeight(): " + source.getHeight() );
+
+        RectF roundRect = new RectF(0, 0, outWidth, outHeight);
+
+        BitmapShader bitmapShader = new BitmapShader(source, BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP);
+        if (scale > 0) {
+            Matrix matrix = new Matrix();
+            matrix.setScale(scale, scale);
+            bitmapShader.setLocalMatrix(matrix);
         }
-        final Paint paint = new Paint();
-        paint.setAntiAlias(true);
-        /**
-         * 产生一个同样大小的画布
-         */
+
+        Bitmap result = pool.get(outWidth, outHeight, Bitmap.Config.ARGB_8888);
+        if (result == null) {
+            result = Bitmap.createBitmap(outWidth, outHeight, Bitmap.Config.ARGB_8888);
+        }
+
         Canvas canvas = new Canvas(result);
-        /**
-         * 首先绘制圆角矩形
-         */
-        RectF rect = new RectF(0, 0, source.getWidth(), source.getHeight());
-        canvas.drawRoundRect(rect, mRadius, mRadius, paint);
-        /**
-         * 使用SRC_IN
-         */
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        /**
-         * 绘制图片
-         */
-        canvas.drawBitmap(source, 0, 0, paint);
+        Paint paint = new Paint();
+        paint.setShader(bitmapShader);
+        paint.setAntiAlias(true);
+        canvas.drawRoundRect(roundRect, mRadius, mRadius, paint);
         return result;
     }
 
     @Override
     public String getId() {
-        return getClass().getName();
+        return getClass().getName() + Math.round(mRadius);
     }
 }
